2da01521f29aaeb55106cf3a486ddb780f5995f2
vigil-senpai <vincent.pradipta@binus.ac.id>
2022-10-30 17:13:59 +0700

    feat: Implement and test basic pagination

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('New Post') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        @foreach ($Posts as $post)
-                            <img src="{{ $post->user->avatarUrl }}" alt="" width="48px" height="48px">
-                            {{ $post->user->username }}
-                            <br>
-                            {{ $post->text }}
-                            <br>
-                            {{ $post->created_at }}
-                            <br><br>
-                        @endforeach
-
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
                     </div>
+                    @endif
+
+                    @foreach ($Posts as $post)
+                    <img src="{{ $post->user->avatarUrl }}" alt="" width="48px" height="48px">
+                    {{ $post->user->username }}
+                    <br>
+                    {{ $post->text }}
+                    <br>
+                    {{ $post->created_at }}
+                    <br><br>
+                    @endforeach
+
+                    {{$Posts->links()}}
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
